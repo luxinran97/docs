1a4a9ef77bdef2e8c368643d7250027655e8e52e
卢鑫冉 <luxinran@luxinrandeMac-mini.local>
2025-11-02 13:39:03 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from './util/auto-gen-siderbar'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "宝黄天",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outline: [2, 6],
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '前端', link: '/front-end/react/index' }
+    ],
+
+    sidebar: [{
+        text: 'React',
+        items: [
+          { text: 'React', link: '/front-end/react/index' },
+          { text: '微信小程序', link: '/front-end/微信小程序/index' },
+        ]
+      }, 
+      {
+        text: '微信小程序',
+        items: [
+          { text: '小程序入门', link: '/front-end/微信小程序/小程序入门' },
+          { text: '小程序入门', link: '/front-end/微信小程序/小程序入门' },
+      ]
+    }],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025 lxr'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "宝黄天"
+  text: "一个文档站点"
+  tagline: 知行合一
+  image:
+    src: /logo.jpg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征A的描述
+  - title: Feature B
+    details:  特征B的描述
+  - title: Feature C
+    details: 特征C的描述
+---
+
